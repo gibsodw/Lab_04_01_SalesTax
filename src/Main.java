55d7da239cd97836bc2e9f1876bc04b01cfc835e
dongi <dongibson633@gmail.com>
2023-09-15 13:52:58 -0400

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main
 {
     public static void main(String[] args) {
+        double purchasePrice = 3.35;
+        double salesTax = 1.05;
+        double total = 0;
+
+        System.out.println("What is the price of your purchase?");
+
+        total = purchasePrice * salesTax;
+
+        System.out.println("The total cost of your purchase at a 5% sales tax is " + total);
 
     }
 }
